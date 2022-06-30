a982c60bdba2b0eaa8329b0cafd760480bd8f1a4
spandan114 <spandanj685@gmail.com>
2022-06-30 10:27:55 +0530

    Generate token function

--- 29-JWT-Auth/controllers/userController.go
+++ 29-JWT-Auth/controllers/userController.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -11,6 +13,7 @@ import (
 	"github.com/spandan114/golang-zero-to-pro/29-JWT-Auth/helpers"
 	"github.com/spandan114/golang-zero-to-pro/29-JWT-Auth/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -35,23 +38,42 @@ func Signup() gin.HandlerFunc {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		count, err := userCollection.CountDocuments(context.Background(), bson.M{"email": user.Email})
+		emailCount, err := userCollection.CountDocuments(context.Background(), bson.M{"email": user.Email})
 
 		if err != nil {
 			log.Panic(err)
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error occur while counting user email"})
+			return
 		}
 
-		count, err := userCollection.CountDocuments(context.Background(), bson.M{"phone": user.PhoneNumber})
+		numCount, err := userCollection.CountDocuments(context.Background(), bson.M{"phone": user.PhoneNumber})
 		if err != nil {
 			log.Panic(err)
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error occur while counting user phone number"})
+			return
 		}
 
-		if count > 0 {
+		if emailCount > 0 || numCount > 0 {
 			ctx.JSON(http.StatusExpectationFailed, gin.H{"error": "This email or phone number alredy exist"})
+			return
 		}
 
+		user.CtratedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserID = user.ID.Hex()
+		token, refreshToken, _ := helpers.GenerateAllTokens(user.Email, user.FirstName, user.LastName, user.UserID, user.UserType)
+		user.Token = token
+		user.RefreshToken = refreshToken
+
+		res, err := userCollection.InsertOne(context.Background(), user)
+
+		if err != nil {
+			msg := fmt.Sprint("User not created yet")
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		ctx.JSON(http.StatusOK, res)
 	}
 }
 
--- 29-JWT-Auth/helpers/tokenHelper.go
+++ 29-JWT-Auth/helpers/tokenHelper.go
@@ -1,1 +1,51 @@
 package helpers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/spandan114/golang-zero-to-pro/29-JWT-Auth/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Uid       string
+	UserTpe   string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "User")
+var secretKey string = os.Getenv("JWT_SEC")
+
+func GenerateAllTokens(email string, firstName string, lastName string, uid string, userType string) (signedToken string, refreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Uid:       uid,
+		UserTpe:   userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaim := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString([]byte(secretKey))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaim).SignedString([]byte(secretKey))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	return token, refreshToken, err
+}
